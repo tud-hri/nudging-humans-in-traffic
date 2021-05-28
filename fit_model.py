4a7442f1770c84badd11d0c9a7e6e028f87a6054
cherepaha <a.zgonnikov@tudelft.nl>
2021-05-28 12:45:16 +0200

    Simplified DDM model added, model fitting added

---
+++ fit_model.py
@@ -0,0 +1,27 @@
+import human_models
+import ddm
+import pandas as pd
+import paranoid as pns
+pns.settings.Settings.set(enabled=False)
+
+data = pd.read_csv('data/pilot0/participant_1_2021_05_22_20_32.csv', sep='\t',
+                   usecols=["participant_id", "d_condition", "tau_condition", "a_condition", "decision", "RT"])
+
+data["is_turn_decision"] = data.decision == "go"
+data = data.drop("decision", axis=1)
+data = data[(data.RT>0) & (data.RT<3)]
+
+conditions = [{"d": d, "tau": tau, "a": a}
+              for d in data.d_condition.unique()
+              for tau in data.tau_condition.unique()
+              for a in data.a_condition.unique()]
+
+training_sample = ddm.Sample.from_pandas_dataframe(df=data,
+                                                   rt_column_name='RT',
+                                                   correct_column_name='is_turn_decision')
+
+model = human_models.HumanModelDDMDynamicDriftFittable()
+
+fitted_model = ddm.fit_adjust_model(sample=training_sample, model=model.model, suppress_output=False)
+print(fitted_model.get_fit_result().value())
+print(fitted_model.get_model_parameters())
--- test_human_model.py
+++ test_human_model.py
@@ -7,7 +7,7 @@ import ddm.plot
 from datetime import datetime
 
 
-def plot_policy(t, distance_gap, time_gap, dtime_gap_dt):
+def plot_policy(t, distance_gap, time_gap, dtime_gap_dt, a):
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
     ax1.plot(t, distance_gap)
     ax1.set_ylabel("distance gap, m", fontsize=12)
@@ -16,7 +16,8 @@ def plot_policy(t, distance_gap, time_gap, dtime_gap_dt):
     ax2.set_ylabel("time gap, s", fontsize=12)
 
     ax3.plot(t, dtime_gap_dt)
-    ax3.set_ylabel("time gap derivative", fontsize=12)
+    ax3.plot(t, a)
+    ax3.set_ylabel("time gap derivative / a", fontsize=12)
 
     ax3.set_xlabel("time, s", fontsize=12)
 
@@ -49,13 +50,14 @@ def test_get_policy_cost():
                            + (av_trajectory.y - human_trajectory.y[0]) ** 2)
     time_gap = distance_gap / av_trajectory.speed
     dtime_gap_dt = utils.get_derivative(t.to_numpy(), time_gap.to_numpy())
+    a = utils.get_derivative(t.to_numpy(), av_trajectory.speed.to_numpy())
 
     start_time = datetime.now()
     policy_cost = get_policy_cost(t, distance_gap, time_gap, dtime_gap_dt)
     print("Model running time: %s" % str(datetime.now() - start_time))
     print("Policy cost: %.2f" % policy_cost)
 
-    plot_policy(t, distance_gap, time_gap, dtime_gap_dt)
+    plot_policy(t, distance_gap, time_gap, dtime_gap_dt, a)
 
 
 test_get_policy_cost()
